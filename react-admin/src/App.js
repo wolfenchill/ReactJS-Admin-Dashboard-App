9af14cbb701b7297a2b826b3dd37d290d2567005
wolfenchill <44275828+wolfenchill@users.noreply.github.com>
2023-02-28 21:22:23 -0800

    colormodecontext to app.js

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,5 +1,14 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <div className="app"> </div>;{" "}
+    </ColorModeContext.Provider>
+  );
 }
 
 export default App;
